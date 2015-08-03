51d3af91b487ee83daf737c944f5c124086216d6
Swati Shrivas <Swati Shrivas>
2015-08-03 19:08:48 +0530

    added print function

--- arithmentic.php
+++ arithmentic.php
@@ -4,6 +4,11 @@ class arithmetic {
 	function add($a,$b){
 		$c = (float)$a + (float)$b;
 		return $c;
-	}     
-
+	}   
+	//added by Swati Shrivas 
+	function print_Str(){
+		echo "Testing github";
+	}
 }
+$obj =  new arithmetic();
+$obj->print_Str();
