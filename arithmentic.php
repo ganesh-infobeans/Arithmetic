276f73eda420535741684289703d0c4d9f50bb2f
himesh-infobeans <himesh.kumar@infobeans.com>
2015-08-03 18:23:54 +0530

    Added add method for testing.

--- arithmentic.php
+++ arithmentic.php
@@ -1,7 +1,13 @@
 <?php 
-
 class arithmetic {
-
-     
+	/**
+	 * Used for addition operation
+	 * @param int $a
+	 * @param int $b
+	 * @return number
+	 */
+	public function add($a = 0,$b = 0) {
+		return ($a+$b);
+	}
 
 }
