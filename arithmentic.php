15174e6ca4da086c2889192806ed60cea302e6a7
unknown <mitesh.shah@IB-SEZ-PC-096.infobeans.in>
2015-08-03 18:55:20 +0530

    multiply function added

--- arithmentic.php
+++ arithmentic.php
@@ -4,7 +4,14 @@ class arithmetic {
 	function add($a,$b){
 		$c = (float)$a + (float)$b;
 		return $c;
-	}     
+	}
+
+	function multiply($m1,$m2){
+		$multiply=$m1*$m2;
+		
+		return $multiply;
+		
+	}	
 
 }
 echo "test";
