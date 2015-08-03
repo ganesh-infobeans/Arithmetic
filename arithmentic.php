b1eb949a1d137717333d6034d3c8566617e5fbbc
Afsar <afsar.khan@infobeans.com>
2015-08-03 19:01:02 +0530

    added substract

--- arithmentic.php
+++ arithmentic.php
@@ -5,6 +5,12 @@ class arithmetic {
 		$c = (float)$a + (float)$b;
 		return $c;
 	}     
+	// Added by shashank shroti
+	function substract($a,$b){
+		$c = (float)$a - (float)$b;
+		return $c;
+	}     
 
 }
-echo "test";
+$subs = new arithmetic();
+$subs->substract(5,3);
