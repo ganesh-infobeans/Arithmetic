fc6a721c4e549bd0219d4bb998087c8f2afdd7cc
himesh-infobeans <himesh.kumar@infobeans.com>
2015-08-03 19:21:29 +0530

    Added division method for testing.

--- arithmentic.php
+++ arithmentic.php
@@ -1,13 +1,58 @@
 <?php 
 class arithmetic {
-	/**
-	 * Used for addition operation
-	 * @param int $a
-	 * @param int $b
-	 * @return number
-	 */
-	public function add($a = 0,$b = 0) {
-		return ($a+$b);
-	}
+	// Added by shashank shroti
+	function add($a,$b){
+		$c = (float)$a + (float)$b;
+		return $c;
 
+	}     
+	// Added by Afsar khan
+	function substract($a,$b){
+		$c = (float)$a - (float)$b;
+		return $c;
+	}     
+
+	//added by Swati Shrivas
+	function print_Str(){
+		echo "Testing github";
+	}
+	
+
+	function multiply($m1,$m2){
+		$multiply=$m1*$m2;
+		
+		return $multiply;
+		
+	}	
+
+
+	
+	// Added by Anand Pandey
+	function modulas($a,$b){
+		$c = (float)$a % (float)$b;
+		return $c;
+	}
+	
+	function division($a=1,$b=1){
+		if(($a != 0) && ($b != 0)) {
+			return (float)$a/(float)$b;
+		} else {
+			return false;
+		}
+	}
 }
+
+$subs = new arithmetic();
+$subs->substract(5,3);
+
+
+$mod = new arithmetic();
+$mod->modulas(10,20);
+
+$obj =  new arithmetic();
+$obj->print_Str();
+
+
+$div = new arithmetic();
+$result = $div->division(10,2);
+var_dump($result);
