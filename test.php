ce4a8bcbda3496d33958637437b9e9725a873f9f
shashank <shashank.shroti@infobeans.com>
2023-04-07 15:44:34 +0530

    update test file

--- test.php
+++ test.php
@@ -1,6 +1,2 @@
 <?php
-<<<<<<< HEAD
-echo 'Year 2023 - updated master branch';
-=======
-echo 'Year 2023 - updated by shashank';
->>>>>>> c48522aa37c28700579dc80813e97e82e67cf2a3
+echo 'Year 2023 - updated by shashank1234';
